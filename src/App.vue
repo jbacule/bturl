1b2d5fd8cef50a48fd42ca96487da41e1b23992c
shujo <44210913+shujo@users.noreply.github.com>
2022-03-17 14:22:15 +0800

    added preloader

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <main>
-    <router-view></router-view>
-  </main>
-  <footer>
-    <div class="footer text-center text-muted py-4">
-      &copy; BtURL 2021 - <a href="https://github.com/shujo/bturl" target="_blank">Github</a>
+  <template v-if="loading">
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+      <div class="text-center">
+        <img class="loading" src="@/assets/loader.gif" alt="loading" height="96" width="96">
+      </div>
     </div>
-  </footer>
+  </template>
+  <template v-else>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer>
+      <div class="footer text-center text-muted py-4">
+        &copy; BtURL 2021 - <a href="https://github.com/shujo/bturl" target="_blank">Github</a>
+      </div>
+    </footer>
+  </template>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   name: "App",
+  setup(){
+    const loading = ref(false)
+
+    const initLoading = () => {
+      loading.value = true
+      setTimeout(() => {
+        loading.value = false
+      }, 2000)
+    }
+
+    initLoading()
+
+    return { loading }
+  }
 };
 </script>
 
